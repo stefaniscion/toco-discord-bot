a722fbc3027b0d3040bf3963303bff99724ae070
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-12-17 21:12:15 +0100

    feat: add environment variable management for discord token

--- toco/main.py
+++ toco/main.py
@@ -1,13 +1,18 @@
+import os
+from dotenv import load_dotenv
 from discord import Intents
 from toco.client import TocoClient
 
 
 def main():
+    load_dotenv()
+    token = os.getenv("DISCORD_TOKEN")
+
     intents = Intents.default()
     intents.message_content = True
 
     client = TocoClient(intents=intents)
-    client.run("")
+    client.run(token)
 
 
 if __name__ == "__main__":
