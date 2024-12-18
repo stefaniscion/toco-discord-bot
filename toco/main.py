ef0486c9a716a9b234b38c2a566f026b9ab223c2
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-12-18 13:53:05 +0100

    refactor: refactor bot initialization and cogs setup in main entry point

--- toco/main.py
+++ toco/main.py
@@ -2,25 +2,31 @@ import os
 import asyncio
 from dotenv import load_dotenv
 from discord import Intents
+from discord.ext import commands
 from toco.bot import TocoBot
 from toco.cogs.utility import UtilityCommands
 
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+intents = Intents.default()
+intents.message_content = True
 
-async def load_bot():
-    load_dotenv()
-    token = os.getenv("DISCORD_TOKEN")
-    intents = Intents.default()
-    intents.message_content = True
 
-    bot = TocoBot(command_prefix="/", intents=intents)
-
+async def setup_cogs(bot: commands.Bot):
+    """Adds the cogs to the bot"""
     await bot.add_cog(UtilityCommands(bot))
+
+
+async def start_bot():
+    """Starts the bot"""
+    bot = TocoBot(command_prefix="/", intents=intents)
+    await setup_cogs(bot)
     await bot.start(token)
 
 
 def main():
-    asyncio.run(load_bot())
+    asyncio.run(start_bot())
 
 
 if __name__ == "__main__":
-    asyncio.run(load_bot())
+    asyncio.run(start_bot())
